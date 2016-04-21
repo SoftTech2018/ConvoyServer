24e851b5ca2ccde77d57d34d057c14985fc5cfa0
eggman1979 <kim.d.rasmussen@live.dk>
2016-04-21 13:38:07 +0200

    tilfæjet dawa klienten og json parseren

---
+++ src/java/dawa/AddressCompleter.java
@@ -0,0 +1,80 @@
+/*
+ DAWA client!
+
+ */
+package convoybackend.dawaClient;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+/**
+ *
+ * @author KimDrewes
+ */
+public class AddressCompleter {
+
+    public ArrayList<String> getAddresses(String addressQuery) throws JSONException {
+        //Instantierer en client
+        Client client = ClientBuilder.newClient();
+        try {
+            // addressQuery.replace("", 0)
+            //Sørger for at mellemrum, æøå osv, kan sendes til REST serveren
+            addressQuery = URLEncoder.encode(addressQuery, "UTF-8");
+        } catch (UnsupportedEncodingException ex) {
+            System.out.println("FUUUCK");
+            Logger.getLogger(AddressCompleter.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        //Kalder GET - get() på target og accepterer JSON
+        Response res = client.target("http://dawa.aws.dk/adresser?q=" + addressQuery).request(MediaType.APPLICATION_JSON).get();
+
+        //Sætter svaret til at være en string
+        String resultString = res.readEntity(String.class);
+        ArrayList<String> addressList = new ArrayList<>();
+
+//     try {
+        //DAWA rest apiet bliver sendt som et stort JSONarray.
+        JSONArray jsonarray = new JSONArray(resultString);
+        for (int i = 0; i < jsonarray.length(); i++) {
+            JSONObject object = jsonarray.getJSONObject(i);
+          
+          
+            
+            JSONObject adresse = object.getJSONObject("adgangsadresse");
+            JSONObject adgangspunkt = adresse.getJSONObject("adgangspunkt");
+            JSONArray koordinat = adgangspunkt.getJSONArray("koordinater");
+            System.out.println("koordinat er " + koordinat);
+            
+        
+           
+        }
+//        System.out.println(resultString);
+
+        //Der køres igennem alle elementerne i array, og der bliver lavet
+        // JSON objekter af disse
+        // Der bliver sorteret så vi kun henter den ressourse der hedder
+        // addressebetegnelse!
+//         for (int i = 0; i < jsonarray.length(); i++) {
+//             JSONObject jsonobject = jsonarray.getJSONObject(i);
+//             String address = jsonobject.getString("adressebetegnelse");
+//       
+//             addressList.add(address);
+//            }       
+//         }catch (JSONException ex) {
+//             System.out.println("Der skete en fejl");
+//            ex.printStackTrace();
+//     }
+//client.close();
+        return addressList;
+    }
+}
---
+++ src/java/diverse/JsonParser.java
@@ -0,0 +1,71 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package diverse;
+
+
+import java.util.ArrayList;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import convoy.db.Spot;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+/**
+ *
+ * @author KimDrewes
+ * Klasse der indeholder  metoder til at udpakke et json Arrays og objekter
+ * Bruges ved at 
+ */
+public class JsonParser {
+     
+ 
+   private ArrayList<Spot> parseJSONArray(JSONArray jArray) throws JSONException{
+          ArrayList<Spot> spots = new ArrayList<>();
+  
+        JSONObject jo;
+        JSONArray ja = jArray;
+            for (int i = 0; i < ja.length(); i++) {
+                jo = ja.getJSONObject(i);
+                Spot spot = new Spot(jo.getInt("id"),jo.getBoolean("addBlue"),jo.getBoolean("food"),jo.getBoolean("wc"),
+                                     jo.getBoolean("bed"),jo.getBoolean("bath"),jo.getBoolean("roadtrain"), jo.getDouble("longitude"), jo.getDouble("latitude"),
+                                    jo.getString("name"), jo.getInt("lastUpdated"), jo.getBoolean("deleted"));
+               spots.add(spot);
+            }
+  return spots;
+    }
+   
+   
+   public  ArrayList<Spot> hentJsonFraServer(){   //Instantierer en client
+    Client client = ClientBuilder.newClient();
+   
+         //TODO INDSÆT SERVERNAVN!
+         //Kalder GET - get() på target og accepterer JSON
+        Response res = client.target("INDSÆT VORES RESTSERVER").request(MediaType.APPLICATION_JSON).get();
+     
+        //Sætter svaret til at være en string
+        String resultString = res.readEntity(String.class);
+        ArrayList<Spot> spots;
+    
+     try {
+         //DAWA rest apiet bliver sendt som et stort JSONarray.
+           JSONArray jsonarray = new JSONArray(resultString);
+           spots = parseJSONArray(jsonarray);
+     }catch(JSONException e){
+         e.printStackTrace();
+     }
+     return null;
+   }
+}
+
+
+
+
+
