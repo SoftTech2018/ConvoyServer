80ad0c178ca1e58397fcaa27aac7e35b5b324d91
Ebbe <Ebbeb2424@gmail.com>
2016-05-12 11:39:18 +0200

    Fjernet kommentarer

--- src/java/brugerautorisation/transport/rmi/ConvoyBackendRmi.java
+++ src/java/brugerautorisation/transport/rmi/ConvoyBackendRmi.java
@@ -27,46 +27,21 @@ public class ConvoyBackendRmi
         System.out.println("Publicerer RMI-tjeneste");
         this.ba = (Brugeradmin) Naming.lookup("rmi://javabog.dk/brugeradmin");
         System.out.println("RMI-tjeneste publiceret"); 
-//    adminLogin("s144842", "xxx");
-//    adminLogin("s144842", "xxx1");
+
     }
     
     public String adminLogin(String brugernavn, String adgangskode) throws Exception{
 
     try {
          Bruger b = ba.hentBruger(brugernavn, adgangskode);      //Bruger forsøges hentes
-         System.out.println("Bruger hentet: " + b.brugernavn);
          String newToken = th.createToken(b.brugernavn);         //Ny token til bruger oprette
          System.out.println(newToken);
-         System.out.println("Token oprettet");
          
          return newToken;
          
         } catch (Exception e) {
             e.getStackTrace();
-            System.out.println("Bruger findes ikke på serveren eller password er forkert");
-//            String bad = "Bruger findes ikke på serveren eller password er forkert";
             return null;
-        }
-    
-    
-    
-    
-    
-//        if (ba.getEkstraFelt("token", adgangskode, brugernavn) == null) {
-//            UUID id = UUID.fromString(brugernavn);
-//            ba.setEkstraFelt(brugernavn, adgangskode, "token", id);   
-//        }
-//        else {
-//            String token = ba.getEkstraFelt(brugernavn, adgangskode, "token").toString();
-//             return ba.hentBruger(brugernavn, adgangskode).toString() + token;
-//        }
-        
-       
-       
-        
-    }
-    
-    
-    
+        }   
+    }  
 }
